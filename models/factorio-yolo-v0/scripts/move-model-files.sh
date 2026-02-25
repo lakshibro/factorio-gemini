cf2b39dd7f3fa3d85bd1f4471b7ac22b9505ef16
LemonNeko <chheese048@gmail.com>
2026-02-25 20:07:54 +0800

    fix: missing files on readme

--- models/factorio-yolo-v0/scripts/move-model-files.sh
+++ models/factorio-yolo-v0/scripts/move-model-files.sh
@@ -10,5 +10,5 @@ fi
 mv ./results/weights/best.pt $model_repo_path/best.pt
 mv ./results/weights/best.onnx $model_repo_path/best.onnx
 cp -f ./notebooks/notebook.ipynb $model_repo_path/notebook.ipynb
-mv ./results/results.png $model_repo_path/results.png
-mv ./results/val_batch0_pred.jpg $model_repo_path/val_batch0_pred.jpg
+cp -f ./results/results.png $model_repo_path/results.png
+cp -f ./results/val_batch0_pred.jpg $model_repo_path/val_batch0_pred.jpg
